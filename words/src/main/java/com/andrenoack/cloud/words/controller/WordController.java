b1b0fe96965ac1110b39f3d567950fb5aa0a4f15
Andre Noack <andre.noack@idealo.de>
2015-12-07 13:22:41 +0100

    server words in JSON format

--- words/src/main/java/com/andrenoack/cloud/words/controller/WordController.java
+++ words/src/main/java/com/andrenoack/cloud/words/controller/WordController.java
@@ -1,7 +1,28 @@
 package com.andrenoack.cloud.words.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by andre.noack on 07.12.15.
  */
+@RestController
 public class WordController {
+	@Value("${words}") String words;
+
+	@RequestMapping("/")
+	public @ResponseBody
+	String getJson() {
+		return "{\"word\":\"" + getWord() + "\"}";
+
+	}
+
+	private String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+	}
+
 }
